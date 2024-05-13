5a486823e883c93dbd02789c442af8f7dd8ac1a7
Abhishek Raj <abhi7cronaldo@gmail.com>
2024-05-13 13:19:31 -0700

    Update options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,7 +1,22 @@
-options = {
-   shiftwidth =  2,
-   tabstop = 2,
-   signcolumn = "auto",
-   spell = false,
-   wrap = false
-}
+  vim.opt.shiftwidth = 2
+  vim.opt.tabstop = 2
+  vim.opt.signcolumn = "auto"
+  vim.opt.spell = false
+  vim.opt.wrap = false
+  vim.opt.number = true
+
+  local map = vim.api.nvim_set_keymap
+
+  local opts = { noremap = true }
+
+  map("v", "<leader>f", "<Plug>(coc-format-selected)", opts)
+  map("n", "<leader>f", "<Plug>(coc-format-selected)", opts)
+
+  vim.cmd([[
+  nnoremap <A-j> :m .+1<CR>==
+  nnoremap <A-k> :m .-2<CR>==
+  inoremap <A-j> <Esc>:m .+1<CR>==gi
+  inoremap <A-k> <Esc>:m .-2<CR>==gi
+  vnoremap <A-j> :m '>+1<CR>gv=gv
+  vnoremap <A-k> :m '<-2<CR>gv=gv
+  ]])
